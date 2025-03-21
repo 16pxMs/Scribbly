a68c5ba8de0d113740d100306183ac2c596985c9
Michael Shumaker <74860626+16pxMs@users.noreply.github.com>
2025-03-21 15:28:33 +0300

    update

--- NewNote.js
+++ NewNote.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const saveButton = document.getElementById("saveButton")
     const clearButton = document.getElementById("clearButton")
     const textInput = document.getElementById("newNoteTextArea")
+    const historyList = documentElement.getElementById("cardHistory")
 
     backButton.addEventListener("click", function() {
         const previousPage = window.open("index.html", "_self")``
@@ -15,10 +16,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
     saveButton.addEventListener("click", function() {
 
-        const noteText = textInput.value
+        const noteText = textInput.value.trim()
 
         // next we check if a user has actually types somwthin into the input box
-
+``
         if (noteText) {
             let notes = JSON.parse(localStorage.getItem("userNotes")) || []
 
@@ -27,6 +28,11 @@ document.addEventListener("DOMContentLoaded", function() {
             localStorage.setItem("userNotes",JSON.stringify(notes))
 
             textInput.value = ""
+
+            const lines = noteText.split('\n')
+            const title = lines[0]
+            const preview = lines.length > 1 ? lines.slice(1).join('\n').substring(0, 50) + '...' : ''
+
 
             console.log("Note saved to localStorage:", noteText)
             // alert("Note saved successfully!")
@@ -38,6 +44,18 @@ document.addEventListener("DOMContentLoaded", function() {
         textInput.value = ""
         console.log("Input field cleared!")
     })
+
+
+    function updateRecentList () {
+        if(!historyList) {
+            console.log("Recents list element not found. Add a div with id 'recentsList' to your HTML.")
+            return
+        }
+    }
+
+
+
+
 
 
 })
